d049e4bc372bf8494597d4bbe8a3260a528459ee
AnyaVasetskaya <annav27@nycstudents.net>
2023-06-12 21:58:48 -0400

    niceee

--- src/TicTacToePanel.java
+++ src/TicTacToePanel.java
@@ -81,54 +81,54 @@ public class TicTacToePanel extends JPanel implements ActionListener {
         if (!controller.getAIWin() && !controller.getPlayerWin()) {
             if (tile.contains("1")) {
                 tile1.setIcon(pSymbol);
-                tile2.setText("");
+                tile1.setText("");
                 //tile1.setFont(new Font("Courier", Font.BOLD,100));
                 tile1.setEnabled(false);
                 controller.setPlayerOrAITaken(1, "player");
             } else if (tile.contains("2")) {
-                tile2.setDisabledIcon(pSymbol);
+                tile2.setIcon(pSymbol);
                 tile2.setText("");
                 //tile2.setFont(new Font("Courier", Font.BOLD,100));
                 tile2.setEnabled(false);
                 controller.setPlayerOrAITaken(2, "player");
             } else if (tile.contains("3")) {
-                tile3.setDisabledIcon(pSymbol);
+                tile3.setIcon(pSymbol);
                 tile3.setText("");
                 //tile3.setFont(new Font("Courier", Font.BOLD,100));
                 tile3.setEnabled(false);
                 controller.setPlayerOrAITaken(3, "player");
             } else if (tile.contains("4")) {
-                tile4.setDisabledIcon(pSymbol);
+                tile4.setIcon(pSymbol);
                 tile4.setText("");
                 //tile4.setFont(new Font("Courier", Font.BOLD,100));
                 tile4.setEnabled(false);
                 controller.setPlayerOrAITaken(4, "player");
             } else if (tile.contains("5")) {
-                tile5.setDisabledIcon(pSymbol);
+                tile5.setIcon(pSymbol);
                 tile5.setText("");
                 //tile5.setFont(new Font("Courier", Font.BOLD,100));
                 tile5.setEnabled(false);
                 controller.setPlayerOrAITaken(5, "player");
             } else if (tile.contains("6")) {
-                tile6.setDisabledIcon(pSymbol);
+                tile6.setIcon(pSymbol);
                 tile6.setText("");
                 //tile6.setFont(new Font("Courier", Font.BOLD,100));
                 tile6.setEnabled(false);
                 controller.setPlayerOrAITaken(6, "player");
             } else if (tile.contains("7")) {
-                tile7.setDisabledIcon(pSymbol);
+                tile7.setIcon(pSymbol);
                 tile7.setText("");
                 //tile7.setFont(new Font("Courier", Font.BOLD,100));
                 tile7.setEnabled(false);
                 controller.setPlayerOrAITaken(7, "player");
             } else if (tile.contains("8")) {
-                tile8.setDisabledIcon(pSymbol);
+                tile8.setIcon(pSymbol);
                 tile8.setText("");
                 //tile8.setFont(new Font("Courier", Font.BOLD,100));
                 tile8.setEnabled(false);
                 controller.setPlayerOrAITaken(8, "player");
             } else if (tile.contains("9")) {
-                tile9.setDisabledIcon(pSymbol);
+                tile9.setIcon(pSymbol);
                 tile9.setText("");
                 //tile9.setFont(new Font("Courier", Font.BOLD,100));
                 tile9.setEnabled(false);
@@ -142,48 +142,48 @@ public class TicTacToePanel extends JPanel implements ActionListener {
         if (!controller.getPlayerWin() && ! controller.getAIWin()) {
             int aiMove = controller.aiMove();
             if (aiMove == 1) {
-                tile1.setDisabledIcon(aiSymbol);
-                tile1.setFont(new Font("Courier", Font.BOLD,70));
+                tile1.setIcon(aiSymbol);
+                tile1.setText("");
                 tile1.setEnabled(false);
                 controller.setPlayerOrAITaken(1, "ai");
             } else if (aiMove == 2) {
-                tile2.setDisabledIcon(aiSymbol);
-                tile2.setFont(new Font("Courier", Font.BOLD,28));
+                tile2.setIcon(aiSymbol);
+                tile2.setText("");
                 tile2.setEnabled(false);
                 controller.setPlayerOrAITaken(2, "ai");
             } else if (aiMove == 3) {
-                tile3.setDisabledIcon(aiSymbol);
-                tile3.setFont(new Font("Courier", Font.BOLD,28));
+                tile3.setIcon(aiSymbol);
+                tile3.setText("");
                 tile3.setEnabled(false);
                 controller.setPlayerOrAITaken(3, "ai");
             } else if (aiMove == 4) {
-                tile4.setDisabledIcon(aiSymbol);
-                tile4.setFont(new Font("Courier", Font.BOLD,28));
+                tile4.setIcon(aiSymbol);
+                tile4.setText("");
                 tile4.setEnabled(false);
                 controller.setPlayerOrAITaken(4, "ai");
             } else if (aiMove == 5) {
-                tile5.setDisabledIcon(aiSymbol);
-                tile5.setFont(new Font("Courier", Font.BOLD,28));
+                tile5.setIcon(aiSymbol);
+                tile5.setText("");
                 tile5.setEnabled(false);
                 controller.setPlayerOrAITaken(5, "ai");
             } else if (aiMove == 6) {
-                tile6.setDisabledIcon(aiSymbol);
-                tile6.setFont(new Font("Courier", Font.BOLD,28));
+                tile6.setIcon(aiSymbol);
+                tile6.setText("");
                 tile6.setEnabled(false);
                 controller.setPlayerOrAITaken(6, "ai");
             } else if (aiMove == 7) {
-                tile7.setDisabledIcon(aiSymbol);
-                tile7.setFont(new Font("Courier", Font.BOLD,28));
+                tile7.setIcon(aiSymbol);
+                tile7.setText("");
                 tile7.setEnabled(false);
                 controller.setPlayerOrAITaken(7, "ai");
             } else if (aiMove == 8) {
-                tile8.setDisabledIcon(aiSymbol);
-                tile8.setFont(new Font("Courier", Font.BOLD,28));
+                tile8.setIcon(aiSymbol);
+                tile8.setText("");
                 tile8.setEnabled(false);
                 controller.setPlayerOrAITaken(8, "ai");
             } else if (aiMove == 9) {
-                tile9.setDisabledIcon(aiSymbol);
-                tile9.setFont(new Font("Courier", Font.BOLD,28));
+                tile9.setIcon(aiSymbol);
+                tile9.setText("");
                 tile9.setEnabled(false);
                 controller.setPlayerOrAITaken(9, "ai");
             }
